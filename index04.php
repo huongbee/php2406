6a695bf33ce37f1934c36f851241e812111f4e49
Huong <huongnguyenak96@gmail.com>
2019-06-26 19:36:35 +0700

    switch

---
+++ index04.php
@@ -0,0 +1,25 @@
+<?php
+
+// $a = 1;
+// echo "<p>
+//         $a
+//         <br>
+//         $a
+//       </p>";
+
+// echo "<p>$a</p>";
+
+$a = 4;
+
+switch($a){
+    case 1: echo 'a = 1'; break;
+    case 2: echo 'a = 2'; break;
+    case 3: echo 'a = 3'; break;
+    default: echo 'a = 4'; break;
+}
+
+
+// $nam = 2019 
+// Nam 2019 la nam dinh hoi
+
+?>
