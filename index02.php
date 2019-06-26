bf84d08ee09e5bf906a9840cd318afd17b0a5930
Huong <huongnguyenak96@gmail.com>
2019-06-26 17:55:36 +0700

    index02

---
+++ index02.php
@@ -0,0 +1,21 @@
+<?php
+
+$str = 'hello'; // string
+$n = 1; // integer
+$m = 1.4; // float
+$bool = false; // boolean: true || false
+$arr = [1,3,5,6]; // array
+
+class Person{ }
+
+$p = new Person(); // object
+// echo gettype($p);
+
+$c = null;  // NULL
+// echo gettype($c);
+
+define('PI', 3.14); // PI: constant name
+echo PI;
+
+
+?>
