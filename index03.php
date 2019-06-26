6dcefd51513c373ad453c65c60a1922c097ca59b
Huong <huongnguyenak96@gmail.com>
2019-06-26 18:25:21 +0700

    operator

---
+++ index03.php
@@ -0,0 +1,29 @@
+<?php
+
+// $a = 'Hello';
+// $b = 'Teo';
+// echo $a.' '.$b;
+// echo $a .= ' '.$b; 
+
+
+// $a = 7;
+// $b = 3;
+// echo $a %= $b; // 1
+
+// echo $a+$b;
+// echo $a+=$b; // 4
+// echo $a; // 4
+// echo $b; // 3
+
+// echo $a + 1; 
+// echo $a++; // $a = $a + 1;
+// echo ++$a; // 3
+// echo $a; // 3
+
+
+// echo $a += 1; //$a = $a + 1;
+
+// echo --$a;
+// echo $a -= 3; //$a = 1-3 = -2
+
+?>
