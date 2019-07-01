098a60894bce736f10cb868588cff5969583f50c
Huong <huongnguyenak96@gmail.com>
2019-07-01 18:40:43 +0700

    index02

---
+++ array/index02.php
@@ -0,0 +1,18 @@
+<?php
+$backend = [ 'PHP', 'MySQL', 'MongoDB', 'NodeJS'];
+
+
+
+// if(in_array('NodeJS', $backend)) echo 'Tim thay';
+// else echo 'Ko tim thay';
+
+
+// $tmp = false;
+// foreach($backend as $sub){
+//     if($sub === 'NodeJS') {
+//         $tmp = true;
+//         break;
+//     }
+// }
+// echo $tmp === true ? 'Tim thay' : 'Ko tim thay';
+?>
