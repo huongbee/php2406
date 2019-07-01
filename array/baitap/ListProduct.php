34a3b48f8e0b903198019b3e9c00001411672beb
Huong <huongnguyenak96@gmail.com>
2019-07-01 19:18:41 +0700

    list

---
+++ array/baitap/ListProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+$list = [
+    [
+        'name' => 'OPPO Reno',
+        'price' => 12990000,
+        'image' => 'https://cdn.tgdd.vn/Products/Images/42/200438/oppo-reno-green-400x460.png'
+    ],
+    [
+        'name' => 'Samsung Galaxy A70',
+        'price' => 9290000,
+        'image' => 'https://cdn.tgdd.vn/Products/Images/42/195012/samsung-galaxy-a70-white-400x460.png'
+    ],
+    [
+        'name' => 'iPhone 6s Plus 32GB',
+        'price' => 9990000,
+        'image' => 'https://cdn.tgdd.vn/Products/Images/42/87846/iphone-6s-plus-32gb-400x460.png'
+    ],
+
+];
+
+?>
