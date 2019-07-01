2ec6020390b48233d2e4709fab818939b5f32043
Huong <huongnguyenak96@gmail.com>
2019-07-01 18:09:54 +0700

    arrSubject

--- array/index.php
+++ array/index.php
@@ -1,10 +1,28 @@
 <?php
-$arrNumber = array(1,3,5,7);
+$arrNumber = array(1,3,5,7,9);
 $arrString = [
     'key' => 'PHP',
     3 => 'MySql',
     'HTML'
 ];
+// for($i = 0; $i < count($arrNumber); $i++){
+//     echo $arrNumber[$i].' ';
+// }
 
+// $backend = [ 'PHP', 'MySQL' ];
+// $frontEnd = [ 'HTML', 'CSS', 'JS', 'Angular' ];
+$arrSubject = [
+    'backend' => [ 'PHP', 'MySQL' ],
+    'frontend' => [ 'HTML', 'CSS', 'JS', 'Angular' ]
+];
+// echo 'Angular';
+
+// $newArr = [
+//     0 => [
+//         12
+//     ]
+// ];
+
+// echo $newArr[0]['key'];
 
 ?>
