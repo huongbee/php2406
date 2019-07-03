ce38e50924d1ed579276e774d7a170d5f9e3c6de
Huong <huongnguyenak96@gmail.com>
2019-07-03 17:50:07 +0700

    init

--- array/baitap/index.php
+++ array/baitap/index.php
@@ -1,10 +1,15 @@
-<?php
-// include 'ListProduct.php';
-// include_once 'ListProduct.php';
-// require 'ListProduct.php';
-require_once 'ListProduct.php';
-
-echo '<pre>';
-    print_r($list);
-echo '</pre>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bai tap</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        
+    </div>
+</body>
+</html>
