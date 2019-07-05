897ece256150864c26e05299a09d3ffea942b8a3
Huong <huongnguyenak96@gmail.com>
2019-07-05 19:06:19 +0700

    GET - POST

--- form/index.php
+++ form/index.php
@@ -8,25 +8,26 @@
 </head>
 <body>
     <h2>Create user account</h2>
-    <form>
-        Name: <input type="text" placeholder="Enter name">
+    <form action="xuly-form.php" method="POST">
+        Name: <input type="text" placeholder="Enter name" name="txtName">
         <br>
-        Email: <input type="email" value="admin@gmail.com">
+        Email: <input type="email" value="admin@gmail.com" name="txtEmail">
         <br>
         Gender: <label> 
-                    <input type="radio" name="gender" value="male">
+                    <input type="radio" name="txtGender" value="male">
                     Male
                 </label>
-                <input type="radio" name="gender" value="female"
+                <input type="radio" name="txtGender" value="female"
                 id="female"> 
                 <label for="female">Female </label>
         <br>
-        Password: <input type="password">
+        Password: <input type="password" name="password">
         <br>
-        Confirm Password: <input type="password">
+        Confirm Password: <input type="password" name="confirm_password">
         <br>
         <button type="reset">Clear</button>
-        <button type="submit">Submit</button>
+        <!-- <button>Submit</button> -->
+        <input type="submit" value="Submit">
     </form>
 </body>
 </html>
---
+++ form/xuly-form.php
@@ -0,0 +1,13 @@
+<?php
+
+// print_r($_GET);
+// echo $_GET['txtName'];
+print_r($_POST);
+
+
+// .php?txtName=name
+// &
+// password=asd
+// &
+// confirm_password=1234
+?>
