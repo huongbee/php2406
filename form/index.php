f9061f3bb7acbb5cd148bbeff63aec80cd9e5519
Huong <huongnguyenak96@gmail.com>
2019-07-05 18:10:55 +0700

    form

---
+++ form/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Create user account</h2>
+    <form>
+        Name: <input type="text">
+        <br>
+        Email: <input type="email">
+        <br>
+        Gender: <input type="radio"> Male
+                <input type="radio"> Female
+        <br>
+        
+        Password: <input type="password">
+        <br>
+        Confirm Password: <input type="password">
+        <br>
+        <button type="reset">Clear</button>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
