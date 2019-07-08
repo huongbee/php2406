42deb9b5d91461fb9e6593c79138b85dfff80ed0
Huong <huongnguyenak96@gmail.com>
2019-07-08 18:08:42 +0700

    bt

--- form/baitap.php
+++ form/baitap.php
@@ -7,18 +7,39 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
-        <input type="number" name="so_a" placeholder="Enter number a" value="<?=$_GET['so_a']?>">
+    <?php
+    // $kq = '';
+    if(isset($_POST['btnCalc'])){
+        $a = $_POST['so_a'];
+        $b = $_POST['so_b'];
+        $pt = $_POST['pheptinh'];
+        if($pt=='+') $kq = $a+$b;
+        elseif($pt=='-') $kq = $a-$b;
+        elseif($pt=='*') $kq = $a*$b;
+        elseif($pt=='/'){
+            if($b==0) $kq = 'Math Error!';
+            else $kq = $a/$b;
+        }
+        elseif($pt=='%'){
+            if($b==0) $kq = 'Math Error!';
+            else $kq = $a%$b;
+        }
+    }
+    
+    ?>
+    <form method="POST">
+        <input type="number" name="so_a" placeholder="Enter number a" value="<?php ?>">
         <select name="pheptinh">
             <option value="+">+</option>
-            <option>-</option>
-            <option>*</option>
-            <option>/</option>
-            <option>%</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+            <option value="%">%</option>
         </select>
-        <input type="number" placeholder="Enter number b">
-        <input type="submit" value="Calc">
-        <input type="number" placeholder="Result" disabled value="<?=5?>">
+        <input type="number" name="so_b" placeholder="Enter number b">
+        <input type="submit" value="Calc" name="btnCalc">
+        <input type="text" placeholder="Result" disabled
+        value="<?=isset($kq) ? $kq : ''?>">
     </form>
 </body>
 </html>
