59c8fde63d543912ba9a8290664d006e8c54972c
Huong <huongnguyenak96@gmail.com>
2019-07-10 19:00:37 +0700

    in_array

--- upload-file/xuly-upload.php
+++ upload-file/xuly-upload.php
@@ -2,18 +2,37 @@
 $name = $_POST['txtName']; // string
 
 $file = $_FILES['avatar']; // array
+// print_r($file);
+// die;
 
-$des = 'images/a.png';
-move_uploaded_file($file['tmp_name'], $des);
-echo 'success';
+if($file['size'] <= 2048000){
+    $arrExt = [
+        'image/png',
+        'image/jpeg',
+        'image/gif'
+    ];
+    if(in_array($file['type'], $arrExt)){
+        $fileName = rand(100000, 999999).'-'.time().'-'.$file['name'];
+        $des = "images/$fileName";
+        move_uploaded_file($file['tmp_name'], $des);
+        echo 'success';
+    }
+    else{
+        die('File not allow!');
+    }
+}
+else{
+    die('File too large!');
+}
+
 
 
 
 
 // print_r($file);
 /**
- * - check file type
- * - check file size
+ * - check file size <= 200kb * 1024 => byte
+ * - check file type  allow: png | jpg | gif | icon
  * - rename file
  */
 
