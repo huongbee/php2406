dbfb9472c16bf2ae31405e8e1e44aaa8667276f8
Huong <huongnguyenak96@gmail.com>
2019-07-10 18:22:05 +0700

    uploadfile

--- upload-file/xuly-upload.php
+++ upload-file/xuly-upload.php
@@ -3,6 +3,13 @@ $name = $_POST['txtName']; // string
 
 $file = $_FILES['avatar']; // array
 
+$des = 'images/a.png';
+move_uploaded_file($file['tmp_name'], $des);
+echo 'success';
+
+
+
+
 // print_r($file);
 /**
  * - check file type
