a48b98116ce132a739e66a096661cc71f00bdae7
Huong <huongnguyenak96@gmail.com>
2019-07-12 18:40:44 +0700

    session

---
+++ session/create_session.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+$_SESSION['cart'] = '30000';
+$_SESSION['user'] = 'admin';
+
+
+?>
---
+++ session/delete_session.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+// delete 1 session 
+// unset($_SESSION['cart']);
+
+// delete all session == exit browser
+session_destroy(); 
+
+
+?>
