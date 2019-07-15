8444261eb3dd72cd142bbe76cdb50154908ef10b
Huong <huongnguyenak96@gmail.com>
2019-07-15 19:30:20 +0700

    bai tap

--- function/index01.php
+++ function/index01.php
@@ -7,6 +7,7 @@ $a = getData();
 // echo $a;
 // echo number_format(2000000);
 /**
+ * 1
  * viết function truyền vào ($length) độ dài, trả về 1 chuỗi kí tự tương ứng với độ dài đó
  * (random 1 string theo độ dài truyền vào)
  *  $length = 20 => cg112e332vjnqwer34qh
@@ -17,8 +18,17 @@ $a = getData();
  *  for from 1->$length
  */
 
-$initString = '9876543210lkjhgfds';
-echo $initString[17];
+// $initString = '9876543210lkjhgfds';
+// echo $initString[17];
 
+
+/**
+ * BT2
+ * Viet 1 function thuc hien cac yeu cau sau phụ thuộc vào tham so đầu vào
+ * Yêu cầu:
+ * a. In ra day cac so chan
+ * b. In ra day cac so le
+ * c. In ra day cac so chia 5 du 4
+ */
 
 ?>
