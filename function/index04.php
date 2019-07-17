da3c48397655f4566986d89b32bd42603f6b4e96
Huong <huongnguyenak96@gmail.com>
2019-07-17 18:32:26 +0700

    checkSNT

--- function/index04.php
+++ function/index04.php
@@ -3,8 +3,15 @@
  * Cho n = 100,
  * In ra dãy các số nguyên tố từ 0 -> n
  * 
- * B1: viet function kiem tra so ngto
+ * B1: viet function kiem tra so a phai la so ngto
  * B2: dung vong lap in day so
  */
-
+function kiemtraSNT($a){
+    if($a == 0 || $a == 1) return false;
+    for($i=2; $i<=sqrt($a); $i++){
+        if($a%$i == 0) return false;
+    }
+    return true;
+}
+// var_dump(kiemtraSNT(6));
 ?>
