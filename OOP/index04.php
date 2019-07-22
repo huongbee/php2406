9a38ee6c28efc6a5d3079e6ca5db37c591d0e226
Huong <huongnguyenak96@gmail.com>
2019-07-22 19:44:00 +0700

    access level

--- OOP/index04.php
+++ OOP/index04.php
@@ -1,12 +1,34 @@
 <?php
-class Parent{
+class Father{
     public $name = 'no_name';
-    protected $money = 20;
-    private $age = 30;
-}
-class Children{
+    protected $money = 20; // friend
+    private $age = 30; // only me
     
+    function getAge(){
+        return $this->age;
+    }
 }
+class Son extends Father{
+    // name
+    // money // protected
+    function getMoney(){
+        return $this->money;
+    }
+    // function getAge(){
+    //     return $this->age; // Undefined
+    // }
+}
+$s = new Son;
+echo $s->getAge()
+
+
+// echo $s->getMoney()
+
+// echo $s->money;
+
+// $s->name = 'Nam';
+// echo $s->name;
+// print_r($s);
 
 
 
