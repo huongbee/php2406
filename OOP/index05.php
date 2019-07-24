d8e9f75512b7179362157413ac6567c18a9ebe59
Huong <huongnguyenak96@gmail.com>
2019-07-24 18:30:16 +0700

    self parent

---
+++ OOP/index05.php
@@ -0,0 +1,29 @@
+<?php
+class Father{
+    public $name = 'no_name';
+    static protected $money = 20; // friend
+    private $age = 30; // only me
+
+    function getMoney(){
+        echo __CLASS__.__FUNCTION__.'<br>';
+        return self::$money; // 20
+    }
+}
+class Son extends Father{
+    static protected $money = 10;
+    
+    function getMoney(){
+        echo __CLASS__.__FUNCTION__.'<br>';
+        return self::$money; // 10
+    }
+    function getMoneyFather(){
+        return parent::getMoney();
+    }
+}
+// $f = new Father;
+// echo $f->getMoney();
+$s = new Son;
+echo $s->getMoney(); // 10
+echo $s->getMoneyFather();  // 20
+
+?>
